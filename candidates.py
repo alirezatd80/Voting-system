41238fe4c447139e75a6f396deaa6d992dded74d
alirezatd80 <153272155+alirezatd80@users.noreply.github.com>
2025-08-11 18:44:52 +0330

    condid

---
+++ candidates.py
@@ -0,0 +1,52 @@
+candidates = [
+    {
+        'id': 0,
+        'name': "Mohammad Hosseini",
+        'number of votes': 0
+    },
+    {
+        'id': 1,
+        'name': "Fatemeh Ahmadi",
+        'number of votes': 0
+    },
+    {
+        'id': 2,
+        'name': "Ali Rezaei",
+        'number of votes': 0
+    },
+    {
+        'id': 3,
+        'name': "Zahra Mohammadi",
+        'number of votes': 0
+    },
+    {
+        'id': 4,
+        'name': "Hassan Nasiri",
+        'number of votes': 0
+    },
+    {
+        'id': 5,
+        'name': "Leila Karimi",
+        'number of votes': 0
+    },
+    {
+        'id': 6,
+        'name': "Amir Abbaszadeh",
+        'number of votes': 0
+    },
+    {
+        'id': 7,
+        'name': "Sara Jafari",
+        'number of votes': 0
+    },
+    {
+        'id': 8,
+        'name': "Reza Ebrahimi",
+        'number of votes': 0
+    },
+    {
+        'id': 9,
+        'name': "Narges Hashemi",
+        'number of votes': 0
+    }
+]
